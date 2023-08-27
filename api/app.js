b428e0bbf4dcca434c4ba476dca2b73c3a8b3a7c
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-08-27 12:59:47 +0200

    API app.js

---
+++ api/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.set("Access-Control-Allow-Headers", "content-type");
+  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.set("Access-Control-Allow-Credentials", "true");
+  res.set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+  next();
+})
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const port = 3001
+
+app.listen(port, () =>
+  console.log(`Verdello API running at port ${port}`)
+);
+
+module.exports = app;
---
+++ web/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+	
+
+	return (
+		<div className='App'>
+			<div>
+			<h1>Vite + React</h1>		
+			</div>
+
+		</div>
+	);
+}
+
+export default App;
